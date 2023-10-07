ec354c44327fedcae35f3f78241efd552e8809c4
Neco86 <33189641+Neco86@users.noreply.github.com>
2023-10-07 17:59:30 +0800

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+
+module.exports = async (req, res) => {
+  const { domain, path } = req.query;
+  const targetUrl = `https://${domain}/${path}`;
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: req.method,
+      headers: req.headers,
+    });
+
+    const responseBody = await response.text();
+    const contentType = response.headers.get("content-type");
+
+    if (contentType && contentType.includes("text/html")) {
+      const modifiedBody = responseBody.replace(
+        /<head>/,
+        '<head><base href="https://' + domain + '/">'
+      );
+      res.setHeader("content-type", contentType);
+      res.send(modifiedBody);
+    } else {
+      res.setHeader("content-type", contentType);
+      res.send(responseBody);
+    }
+  } catch (error) {
+    res.status(500).send("Error processing the request.");
+  }
+};
